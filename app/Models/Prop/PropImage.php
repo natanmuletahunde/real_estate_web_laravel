40f6820d2ad42c7f753ac4b24baeaec1adb6476d
pivnati <natanmuleta911@gmail.com>
2024-11-05 10:49:18 -0700

    create the propertyImage model in the prop folder

---
+++ app/Models/Prop/PropImage.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models\Prop;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PropImage extends Model
+{
+    
+
+    protected $table = 'prop_image';
+    protected $fillable = [
+        'Prop_id',
+        'image',
+       
+    ];
+    public $timestamp = true;
+}
