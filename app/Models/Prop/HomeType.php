bbbb49112b5f7dc5c917530e21d22cb97adf4514
pivnati <natanmuleta911@gmail.com>
2024-11-09 10:58:12 -0700

    create the hometype model

---
+++ app/Models/Prop/HomeType.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models\Prop;
+
+use Illuminate\Database\Eloquent\Model;
+
+class HomeType extends Model
+{
+    //
+    protected $table = 'hometypes';
+    protected $fillable = [
+        'id',
+        'hometypes',
+    
+    ];
+    public $timestamp = true;
+}
