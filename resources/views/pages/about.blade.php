650eacee1b75854e393ca5f2a9522d255cf0fc98
pivnati <natanmuleta911@gmail.com>
2024-11-09 20:39:35 -0700

    modify the about page

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 
-<div class="site-blocks-cover inner-page-cover overlay" style="background-image: url(images/hero_bg_2.jpg);" data-aos="fade" data-stellar-background-ratio="0.5">
+<div class="site-blocks-cover inner-page-cover overlay" style="background-image: url({{asset('assets/images/hero_bg_2.jpg')}});" data-aos="fade" >
       <div class="container">
         <div class="row align-items-center justify-content-center text-center">
           <div class="col-md-10">
@@ -17,7 +17,7 @@
       <div class="container">
         <div class="row">
           <div class="col-md-6" data-aos="fade-up" data-aos-delay="100">
-            <img src="images/about.jpg" alt="Image" class="img-fluid">
+            <img src="{{asset('assets/images/about.jpg')}}" alt="Image" class="img-fluid">
           </div>
           <div class="col-md-5 ml-auto"  data-aos="fade-up" data-aos-delay="200">
             <div class="site-section-title">
@@ -45,7 +45,7 @@
           <div class="col-md-6 col-lg-4 mb-5 mb-lg-5"  data-aos="fade-up" data-aos-delay="200">
             <div class="team-member">
 
-              <img src="images/person_1.jpg" alt="Image" class="img-fluid rounded mb-4">
+              <img src="{{asset('assets/images/person_1.jpg')}}" alt="Image" class="img-fluid rounded mb-4">
 
               <div class="text">
 
@@ -65,7 +65,7 @@
           <div class="col-md-6 col-lg-4 mb-5 mb-lg-5"  data-aos="fade-up" data-aos-delay="300">
             <div class="team-member">
 
-              <img src="images/person_2.jpg" alt="Image" class="img-fluid rounded mb-4">
+              <img src="{{asset('assets/images/person_2.jpg')}}" alt="Image" class="img-fluid rounded mb-4">
 
               <div class="text">
 
@@ -85,7 +85,7 @@
           <div class="col-md-6 col-lg-4 mb-5 mb-lg-5"  data-aos="fade-up" data-aos-delay="400">
             <div class="team-member">
 
-              <img src="images/person_3.jpg" alt="Image" class="img-fluid rounded mb-4">
+              <img src="{{asset('assets/images/person_3.jpg')}}" alt="Image" class="img-fluid rounded mb-4">
 
               <div class="text">
 
