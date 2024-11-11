cf8ed902dddfb8eb289df0b15f0fef2a1804f7e3
pivnati <natanmuleta911@gmail.com>
2024-11-11 14:37:12 -0700

    create the filename called displayrequest.blade.php  in the users folder

--- app/Http/Controllers/Users/UsersController.php
+++ app/Http/Controllers/Users/UsersController.php
@@ -4,11 +4,12 @@ namespace App\Http\Controllers\Users;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Prop\AllRequest;
 
 class UsersController extends Controller
 {
 
     public  function allRequests(){
-
+         $allRequests = 
     }
 }
