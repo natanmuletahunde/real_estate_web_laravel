65d9da28ce6dcdad03ed8fdbd329677277724e53
pivnati <natanmuleta911@gmail.com>
2024-11-11 14:45:32 -0700

    fetch the authenticated user's ID.  from the db

--- app/Http/Controllers/Users/UsersController.php
+++ app/Http/Controllers/Users/UsersController.php
@@ -5,11 +5,13 @@ namespace App\Http\Controllers\Users;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Prop\AllRequest;
+use Illuminate\Support\Facades\Auth;
 
 class UsersController extends Controller
 {
 
     public  function allRequests(){
-         $allRequests = 
+         $allRequests =  AllRequest::where('user_id',Auth::user()->id)->get();
+         return view('users.displayrequests', compact('allrequests'));
     }
 }
