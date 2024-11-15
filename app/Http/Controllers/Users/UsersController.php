3ac47f69e12b3aab2f143196b76fbb1342e7ef5f
pivnati <natanmuleta911@gmail.com>
2024-11-15 16:51:04 -0700

    crete the function in thw usercontroller

--- app/Http/Controllers/Users/UsersController.php
+++ app/Http/Controllers/Users/UsersController.php
@@ -4,11 +4,14 @@ namespace App\Http\Controllers\Users;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Prop\AllRequest;
+use Illuminate\Support\Facades\Auth;
 
 class UsersController extends Controller
 {
     public function allRequests()
     {
-        
+         $allRequests = AllRequest::where('user_id',Auth::user()->id)->get();  
+         return view('users.displayrequests', compact('allRequests')); 
     }
 }
