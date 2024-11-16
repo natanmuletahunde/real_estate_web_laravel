4d79804b9542b3541db8ea9e137e712717f53f69
pivnati <natanmuleta911@gmail.com>
2024-11-16 00:14:35 -0700

    make the logout from the page expired

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -74,25 +74,26 @@
                         <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                             {{ Auth::user()->name }}
                         </a>
-
-                        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-
-                            <a class="dropdown-item" href="{{ route('all.requests') }}">
-                                All Request
-                            </a>
-                            <a class="dropdown-item" href="{{ route(' all.saved.props') }}">
-                                All Saved Properties
-                            </a>
-                            <a class="dropdown-item" href="{{ route('logout') }}"
-                                onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                {{ __('Logout') }}
-                            </a>
-
                             <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                @csrf
+                            @csrf
+                            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('all.requests') }}">
+                                    All Request
+                                </a>
+                                <a class="dropdown-item" href="{{ route('all.saved.props') }}">
+                                    All Saved Properties
+                                </a>
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                    onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                </a>
+                                </div>
                             </form>
-                        </div>
+
+
+
+                       
                     </li>
                     @endguest
                 </ul>
