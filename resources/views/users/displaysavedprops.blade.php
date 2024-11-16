2d5b8fc4b865209d0da3421affe4b57881c5403f
pivnati <natanmuleta911@gmail.com>
2024-11-16 14:34:33 -0700

    modfiy the displaysavedprops.php

--- resources/views/users/displaysavedprops.blade.php
+++ resources/views/users/displaysavedprops.blade.php
@@ -31,9 +31,7 @@
        <div class="col-m    d-6 col-lg-4 mb-4">
          <div class="property-entry h-100">
            <a href="{{route('single.prop', $relatedProp->id )}}" class="property-thumbnail">
-             <div class="offer-type-wrap">
-               <span class="offer-type  bg-danger">{{$relatedProp->type}}</span>
-             </div>
+             
              <img src="{{asset('assets/images/'.$relatedProp->image. '')}}" alt="Image" class="img-fluid">
            </a>
            <div class="p-4 property-body">
@@ -41,30 +39,16 @@
              <h2 class="property-title"><a href="{{route('single.prop', $relatedProp->id )}}">{{$relatedProp->title}}</a></h2>
              <span class="property-location d-block mb-3"><span class="property-icon icon-room"></span>{{$relatedProp->location}}</span>
              <strong class="property-price text-primary mb-3 d-block text-success">{{$relatedProp->price}}</strong>
-             <ul class="property-specs-wrap mb-3 mb-lg-0">
-               <li>
-                 <span class="property-specs">Beds</span>
-                 <span class="property-specs-number">{{$relatedProp->beds}}<sup>+</sup></span>
-
-               </li>
-               <li>
-                 <span class="property-specs">Baths</span>
-                 <span class="property-specs-number">{{$relatedProp->baths}}</span>
-
-               </li>
-               <li>
-                 <span class="property-specs">{{$relatedProp->sq_ft}}</span>
-                 <span class="property-specs-number">7,000</span>
-
-               </li>
-             </ul>
+            
 
            </div>
          </div>
        </div>
        @endforeach
-       @endif
-
+      <!-- #region -->
+      @else
+      <h3 class = "alert alert-success"> There are any  saved  Properties just yet</h3>
+      @endif
        <footer class="site-footer">
          <div class="container">
            <div class="row">
