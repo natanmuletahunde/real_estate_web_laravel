5f00e826e42638f542d6380e662d2fe5e0fc2c6e
pivnati <natanmuleta911@gmail.com>
2024-11-16 21:53:22 -0700

    put all the route in the group route

--- routes/web.php
+++ routes/web.php
@@ -16,35 +16,36 @@ Auth::routes();
 Route::get('/home', [PropertiesController::class, 'index'])->name('home');
 // group route
 Route::group(['prefix'=> 'props'] , function(){
-
-});
-
-
-Route::get('props/prop-details/{id}', [PropertiesController::class, 'single'])->name('single.prop');
+   
+    
+Route::get('prop-details/{id}', [PropertiesController::class, 'single'])->name('single.prop');
 // inserting request 
-Route::post('props/prop-details/{id}', [PropertiesController::class, 'insertRequests'])->name('insert.request');
+Route::post('prop-details/{id}', [PropertiesController::class, 'insertRequests'])->name('insert.request');
 
 // saving props
-Route::post('props/saved-props/{id}', [PropertiesController::class, 'saveProps'])->name('save.prop');
+Route::post('saved-props/{id}', [PropertiesController::class, 'saveProps'])->name('save.prop');
 
 // displying props by rent and buy
 
-Route::get('props/type/Buy', [PropertiesController::class, 'propsBuy'])->name('buy.prop');
+Route::get('type/Buy', [PropertiesController::class, 'propsBuy'])->name('buy.prop');
 
-Route::get('props/type/rent', [PropertiesController::class, 'propsRent'])->name('rent.prop');
+Route::get('type/rent', [PropertiesController::class, 'propsRent'])->name('rent.prop');
   
 
 // displying props by  hometype
 
 
-Route::get('props/home-type/{hometype}', [PropertiesController::class, 'displayByHomeType'])->name('display.prop.hometytpe');
+Route::get('home-type/{hometype}', [PropertiesController::class, 'displayByHomeType'])->name('display.prop.hometytpe');
 
 
 // displying props by  price asc and desc 
    
 
-Route::get('props/price-asc', [PropertiesController::class, 'priceAsc'])->name('price.asc.prop');
-Route::get('props/price-desc', [PropertiesController::class, 'priceDesc'])->name('price.desc.prop');
+Route::get('price-asc', [PropertiesController::class, 'priceAsc'])->name('price.asc.prop');
+Route::get('price-desc', [PropertiesController::class, 'priceDesc'])->name('price.desc.prop');
+});
+
+
 
 // display contact and about pages
 
