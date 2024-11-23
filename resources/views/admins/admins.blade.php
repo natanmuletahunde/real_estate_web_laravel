9993c7dbcb1d21663eee157dc72ace0d00c1fd3c
pivnati <natanmuleta911@gmail.com>
2024-11-23 15:48:11 -0700

    make check that are the data in the session inside admin

--- resources/views/admins/admins.blade.php
+++ resources/views/admins/admins.blade.php
@@ -4,32 +4,39 @@
 
 
 <div class="row">
-        <div class="col">
-          <div class="card">
+    <div class="col">
+        <div class="card">
             <div class="card-body">
-              <h5 class="card-title mb-4 d-inline">Admins</h5>
-             <a  href="{{route('admins.create')}}" class="btn btn-primary mb-4 text-center float-right">Create Admins</a>
-              <table class="table">
-                <thead>
-                  <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">username</th>
-                    <th scope="col">email</th>
-                  </tr>
-                </thead>
-                <tbody>
-                
-                </tbody>
-                @foreach ($allAdmins as  $allAdmin )
-                <tr>
-                    <th scope="row">{{$allAdmin->id}}</th>
-                    <td>{{$allAdmin->name}}</td>
-                    <td>{{$allAdmin->email}}</td>
-                </tr>
-                @endforeach
-              </table> 
+                <div class="container">
+                    @if (\Session::has('success'))
+                    <div class="alert alert-success">
+                        <p>{!!\Session::get('success') !!}</p>
+                    </div>
+                    @endif
+                </div>
+                <h5 class="card-title mb-4 d-inline">Admins</h5>
+                <a href="{{route('admins.create')}}" class="btn btn-primary mb-4 text-center float-right">Create Admins</a>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">username</th>
+                            <th scope="col">email</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                    </tbody>
+                    @foreach ($allAdmins as $allAdmin )
+                    <tr>
+                        <th scope="row">{{$allAdmin->id}}</th>
+                        <td>{{$allAdmin->name}}</td>
+                        <td>{{$allAdmin->email}}</td>
+                    </tr>
+                    @endforeach
+                </table>
             </div>
-          </div>
         </div>
-      </div>
-      @endsection
+    </div>
+</div>
+@endsection
