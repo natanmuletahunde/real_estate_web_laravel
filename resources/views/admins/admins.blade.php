ecb48d5cda19ac42adfd91a563af300624b138fa
pivnati <natanmuleta911@gmail.com>
2024-11-23 14:24:22 -0700

    create the amins.blade.html in the admins folder

---
+++ resources/views/admins/admins.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.admin')
+
+@section('content')
+
+
+<div class="row">
+        <div class="col">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title mb-4 d-inline">Admins</h5>
+             <a  href="create-admins.html" class="btn btn-primary mb-4 text-center float-right">Create Admins</a>
+              <table class="table">
+                <thead>
+                  <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">username</th>
+                    <th scope="col">email</th>
+                  </tr>
+                </thead>
+                <tbody>
+                
+                </tbody>
+                @foreach ($allAdmins as  $allAdmin )
+                <tr>
+                    <th scope="row">{{$allAdmin->id}}</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                </tr>
+                @endforeach
+              </table> 
+            </div>
+          </div>
+        </div>
+      </div>
+      @endsection
