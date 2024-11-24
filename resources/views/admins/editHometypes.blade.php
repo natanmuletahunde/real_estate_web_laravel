6581f7a80cb5291b01ca77d7b9ad1273571ed498
pivnati <natanmuleta911@gmail.com>
2024-11-24 12:08:14 -0700

    creat  the edithometypes in the controller

---
+++ resources/views/admins/editHometypes.blade.php
@@ -0,0 +1,28 @@
+
+@extends('layouts.admin')
+
+@section('content')
+<div class="row">
+        <div class="col">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title mb-5 d-inline">Update Hometypes</h5>
+          <form method="POST" action="" enctype="multipart/form-data">
+                <!-- Email input -->
+                <div class="form-outline mb-4 mt-4">
+                  <input type="text" value="{{$hometype->hometypes}}" name="hometypes" id="form2Example1" class="form-control" placeholder="hometype" />
+                 
+                </div>
+
+      
+                <!-- Submit button -->
+                <button type="submit" name="submit" class="btn btn-primary  mb-4 text-center">update</button>
+
+          
+              </form>
+
+            </div>
+          </div>
+        </div>
+      </div>
+@endsection
