4ba117d3eb8e16f698e7cfde62563f6cf0f208ce
pivnati <natanmuleta911@gmail.com>
2024-11-24 12:32:21 -0700

    create the error message in the update page

--- resources/views/admins/editHometypes.blade.php
+++ resources/views/admins/editHometypes.blade.php
@@ -1,29 +1,32 @@
-
 @extends('layouts.admin')
 
 @section('content')
 <div class="row">
-        <div class="col">
-          <div class="card">
+    <div class="col">
+        <div class="card">
             <div class="card-body">
-              <h5 class="card-title mb-5 d-inline">Update Hometypes</h5>
-          <form method="POST" action="{{route('hometypes.update', $hometype->id)}}" enctype="multipart/form-data">
-                <!-- Email input -->
-                 @csrf
-                <div class="form-outline mb-4 mt-4">
-                  <input type="text" value="{{$hometype->hometypes}}" name="hometypes" id="form2Example1" class="form-control" placeholder="hometype" />
-                 
-                </div>
+                <h5 class="card-title mb-5 d-inline">Update Hometypes</h5>
+                <form method="POST" action="{{route('hometypes.update', $hometype->id)}}" enctype="multipart/form-data">
+                    <!-- Email input -->
+                    @csrf
+                    <div class="form-outline mb-4 mt-4">
+                        <input type="text" value="{{$hometype->hometypes}}" name="hometypes" id="form2Example1" class="form-control" placeholder="hometype" />
+                        @error('hometypes')
+                        <span class="text-danger" role="alert">
+                            <strong>{{$message}}</strong>
+                        </span>
+                        @enderror
+                    </div>
 
-      
-                <!-- Submit button -->
-                <button type="submit" name="submit" class="btn btn-primary  mb-4 text-center">update</button>
 
-          
-              </form>
+                    <!-- Submit button -->
+                    <button type="submit" name="submit" class="btn btn-primary  mb-4 text-center">update</button>
+
+
+                </form>
 
             </div>
-          </div>
         </div>
-      </div>
+    </div>
+</div>
 @endsection
